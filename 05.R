ec019da791db3d2b9bb6dc851f44a081eadc6978
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-06-30 10:28:27 +0200

    Simplify Day 5

--- 05.R
+++ 05.R
@@ -4,35 +4,23 @@ read_file("input05") %>%
   str_split("\\n\\n", simplify = TRUE) %>%
   str_split("\\n") -> input
 
-head(input[[1]], -1) %>%
-  str_replace_all("[\\[\\]]", " ") %>%
-  str_replace_all(" {4}", " .") %>%
-  str_replace_all(" {3}", " ") %>%
-  str_replace_all("^ | $", "") %>%
-  tibble %>%
-  separate(col = ".", into = str_extract_all(tail(input[[1]], 1), "\\d",
-                                             simplify = TRUE), sep = " ") %>%
-  pivot_longer(everything()) %>%
-  filter(value != ".") -> in_cranes
+input[[1]] %>%
+  str_split("") %>%
+  do.call(what = rbind) %>%
+  t -> mat
+mat <- mat[seq(2, nrow(mat), 4),ncol(mat):1]
+map(1:nrow(mat), ~tail(mat[.,mat[.,] != " "], -1)) %>%
+  `names<-`(mat[,1]) -> cranes
 
-cranes <- list()
-for (i in sort(unique(in_cranes$name))) {
-  cranes[[i]] <- in_cranes %>%
-    filter(name == i) %>%
-    pull(value) %>%
-    rev
-}
-cranes2 <- cranes
-
-head(input[[2]], -1) %>%
+input[[2]] %>%
+  head(-1) %>%
   str_extract_all("\\d+") %>%
   map(as.integer) -> insts
 
+cranes2 <- cranes
 for (inst in insts) {
-  for (i in seq_len(inst[1])) {
-    cranes[[inst[3]]] <- c(cranes[[inst[3]]], tail(cranes[[inst[2]]], 1))
-    cranes[[inst[2]]] <- head(cranes[[inst[2]]], -1)
-  }
+  cranes[[inst[3]]] <- c(cranes[[inst[3]]], tail(cranes[[inst[2]]], inst[1]) %>% rev)
+  cranes[[inst[2]]] <- head(cranes[[inst[2]]], -inst[1])
   cranes2[[inst[3]]] <- c(cranes2[[inst[3]]], tail(cranes2[[inst[2]]], inst[1]))
   cranes2[[inst[2]]] <- head(cranes2[[inst[2]]], -inst[1])
 }
