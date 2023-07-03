5eaf5678ea87cb7acb5d79d7c40185b3a2280ac2
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-07-03 16:48:03 +0200

    Simplify Day 8

---
+++ 08.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+
+read_lines("input08") %>%
+  str_split("") %>%
+  map(as.integer) %>%
+  do.call(what = rbind) -> heights
+
+visible <- 0
+for (i in 1:nrow(heights))
+  for (j in 1:ncol(heights))
+    if (all(heights[i,j] > head(heights[,j], i - 1)) || 
+        all(heights[i,j] > head(heights[i,], j - 1)) ||
+        all(heights[i,j] > tail(heights[,j], -i)) ||
+        all(heights[i,j] > tail(heights[i,], -j)))
+      visible <- visible + 1
+print(visible)
+
+score <- 0
+hh <- cbind(max(heights) + 1,
+            rbind(max(heights) + 1, heights, max(heights) + 1),
+            max(heights) + 1)
+for (i in 2:(nrow(hh) - 1))
+  for (j in 2:(ncol(hh) - 1)) {
+    left <- which(rev(head(hh[i,], j - 1)) >= hh[i,j])[1]
+    right <- which(tail(hh[i,], -j) >= hh[i,j])[1]
+    up <- which(rev(head(hh[,j], i - 1)) >= hh[i,j])[1]
+    down <- which(tail(hh[,j], -i) >= hh[i,j])[1]
+    score <- max(score, min(left, j - 2) *
+                   min(right, ncol(hh) - j - 1) *
+                   min(up, i - 2) *
+                   min(down, nrow(hh) - i - 1))
+  }
+print(score)
+
+# Vectorized alternative
+
+visible <- heights * FALSE
+score <- heights * 0 + 1
+for (k in 1:4) {
+  # Part 1
+  visible[,1] <- TRUE
+  visible[,2] <- visible[,2] | heights[,2] > heights[,1]
+  for (j in 3:ncol(heights))
+    visible[,j] <- visible[,j] | apply(heights[,j] > heights[,1:(j - 1)], 1, all)
+  # Part 2
+  local_score <- score * 0
+  for (j in 1:(ncol(heights) - 2))
+    local_score[,j] <- pmin(apply(heights[,j] <= heights[,(j + 1):ncol(heights)], 1,
+                                  \(x) which(x)[1]), ncol(heights) - j, na.rm = TRUE)
+  local_score[,ncol(heights) - 1] <- heights[,ncol(heights) - 1] > heights[,ncol(heights)]
+  score <- score * local_score
+  # Rotate matrices
+  heights <- t(apply(heights, 2, rev))
+  visible <- t(apply(visible, 2, rev))
+  score <- t(apply(score, 2, rev))
+}
+print(c(sum(visible), max(score)))
