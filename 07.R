de90747247d99455bce07921e5004945cf844a06
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-07-03 09:45:44 +0200

    Simplify Day 7

---
+++ 07.R
@@ -0,0 +1,20 @@
+insts <- read_lines("input07")
+
+sizes <- list()
+path <- "/"
+for (inst in tail(insts, -1)) {
+  if (str_detect(inst, "^\\$ cd \\.\\.")) {
+    path <- head(path, -1)
+  } else if (str_detect(inst, "^\\$ cd ")) {
+    dir <- str_sub(inst, start = 6)
+    path <- c(path, dir)
+  } else if (str_detect(inst, "^\\d+")) {
+    size <- str_extract(inst, "^\\d+") %>% as.integer
+    for (i in 1:length(path))
+      sizes[[paste0(path[1:i], collapse = "/")]] <-
+        sum(sizes[[paste0(path[1:i], collapse = "/")]], size)
+  }
+}
+sizes <- unlist(sizes)
+sum(sizes[sizes <= 100000])
+min(sizes[sizes >= sizes["/"] - 40000000])
